47b28f4781d4daa7c34373b5d75b459853616d05
MigueBellido <mfb.bellido@gmail.com>
2022-07-19 17:31:11 -0300

    FIN DE FUNCION SUMAR

--- FuncionSuma/src/main/java/FuncionSuma.java
+++ FuncionSuma/src/main/java/FuncionSuma.java
@@ -1,5 +1,27 @@
+//*Crear una función con tres parámetros que sean números que se suman entre sí.*//
+
+//*Llamar a la función en el main y darle valores.*//
+
+
+
 public class FuncionSuma {
+
+    int valor1=5;
+    int valor2=6;
+    int valor3=7;
+    int resultado;
+
+    public void operar(){
+        resultado=valor1+valor2+valor3;
+    }
+    public void imprimir(){
+        System.out.println(resultado);
+    }
+
     public static void main(String[] args) {
-        
+        FuncionSuma suma=new FuncionSuma();
+        suma.operar();
+        suma.imprimir();
+
     }
 }
